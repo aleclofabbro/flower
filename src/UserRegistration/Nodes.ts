f21878fe301ef9eba9d77c516b5efdd8332c6687
aleclofabbro <alessandro.giansanti@gmail.com>
2019-04-16 17:53:48 +0200

    UseCase+Flow

---
+++ src/UserRegistration/Nodes.ts
@@ -0,0 +1,14 @@
+// import  UserRegistration from './UseCase';
+// import { Channels } from '../ChannelsTypeDef';
+
+// export const start = (ch: Channels<UserRegistration>) => {
+
+//   ch('RegistrationInCharge', (msg, _) => {
+//     return {
+//       registrationRequestId: msg.registrationRequestId,
+//       attempt: 1,
+//       email: msg.email,
+//       userName: msg.userName
+//     }
+//   })
+// }
---
+++ src/UserRegistration/Types.ts
@@ -0,0 +1,36 @@
+export type Email = string
+export type UserName = string
+export type RegistrationRequestId = string
+
+export enum May__UserName_Unavailable__Or__Contact_Already_Registered {
+  UserNameUnavailable,
+  ContactAlreadyRegistered
+}
+
+export enum May__Not_Match__Or__Already_Confirmed {
+  NoMatch,
+  RegistrationAlreadyConfirmed
+}
+
+export interface UserRegistrationRequest {
+  userName: UserName
+  email: Email
+}
+
+export interface HasRegistrationRequestFailReason {
+  reason: May__UserName_Unavailable__Or__Contact_Already_Registered
+}
+
+export interface HasConfirmationRequestFailReason {
+  reason: May__Not_Match__Or__Already_Confirmed
+}
+
+export interface HasRegistrationRequestId {
+  registrationRequestId: RegistrationRequestId
+}
+
+export type RegistrationRecord = UserRegistrationRequest & HasRegistrationRequestId
+export type RegistrationConfirmRecord = RegistrationRecord & { attempt: number }
+
+export type ScheduleConfirmationTimeout = RegistrationConfirmRecord & { timeout: number, u?: 's' | 'm' | 'h' | 'd' }
+
